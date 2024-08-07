95f1beb2b7f825bcaae15295bc837dc6c3144ce0
Shiva Saini <factwiths@gmail.com>
2024-08-07 19:13:50 +0530

    event unused bug

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+  const handleClose = (_event: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === 'clickaway') {
       return;
     }
